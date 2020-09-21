887d18512290745fce9ed16d1175ff6be5181637
Nguyen Le <nle20959@gmail.com>
2020-09-21 20:44:39 +0700

    design

--- models/Idiom.js
+++ models/Idiom.js
@@ -11,13 +11,6 @@ const idiomSchema = Schema(
       ref: "User",
     },
     images: [String],
-    reactions: {
-      laugh: { type: Number, default: 0 },
-      sad: { type: Number, default: 0 },
-      like: { type: Number, default: 0 },
-      love: { type: Number, default: 0 },
-      angry: { type: Number, default: 0 },
-    },
     reviewCount: { type: Number, default: 0 },
     isDeleted: { type: Boolean, default: false, select: false },
   },
